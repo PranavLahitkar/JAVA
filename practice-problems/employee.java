0f75b3f7de4d07b4816167ce6d00bf651614d916
Pranav Arun Lahitkar <109038291+PranavLahitkar@users.noreply.github.com>
2024-03-23 00:20:46 +0530

    Add files via upload

---
+++ practice-problems/employee.java
@@ -0,0 +1,13 @@
+package demo1.collectionList;
+
+public class employee {
+    int emp_id;
+    String emp_name;
+    int emp_salary;
+
+    public employee(int emp_id, String emp_name, int emp_salary) {
+        this.emp_id = emp_id;
+        this.emp_name = emp_name;
+        this.emp_salary = emp_salary;
+    }
+}
---
+++ practice-problems/student.java
@@ -0,0 +1,14 @@
+package demo1.collectionList;
+
+public class student {
+    int id;
+    String name;
+    String add;
+
+    public student(int id, String name, String add) {
+        this.id = id;
+        this.name = name;
+        this.add = add;
+    }
+
+}
