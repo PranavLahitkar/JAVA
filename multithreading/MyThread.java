c418b66059d748510c9a62f317076b13c1e1d3e3
Pranav Arun Lahitkar <109038291+PranavLahitkar@users.noreply.github.com>
2024-05-04 22:37:52 +0530

    basic program to demonstrate multi threading

---
+++ multithreading/MyThread.java
@@ -0,0 +1,25 @@
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + ": " + i);
+            try {
+                Thread.sleep(1000); // Sleep for 1 second
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+        
+        t1.setName("Thread 1");
+        t2.setName("Thread 2");
+        
+        t1.start();
+        t2.start();
+    }
+}
